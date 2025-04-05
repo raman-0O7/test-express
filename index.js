58398ce3b971c9db434bc35193224c4e297d5145
Raman Kumar <rku.430@gmail.com>
2025-04-05 14:33:15 +0530

    INit

---
+++ index.js
@@ -0,0 +1,10 @@
+const expresss = require('express');
+const app = expresss();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
